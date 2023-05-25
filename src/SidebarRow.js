e53f974f8bc7d701fc93b16e062de63d2fa2596c
Preethi S <95564341+Preethi-485@users.noreply.github.com>
2023-05-25 10:33:14 +0530

    Update SidebarRow.js

--- src/SidebarRow.js
+++ src/SidebarRow.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import "./SidebarRow.css";
 
-
 function SidebarRow({selected, Icon, title}){ /*if we don't capitalise this Icon, it doen't work*/
     return(
         <div className={`SidebarRow ${selected && "selected"}`}>
             <Icon className="sidebarRow_icon"/>
             <h3 className="sidebarRow_title">{title}</h3>
-
-            
-        </div>
+         </div>
     );
 }
 export default SidebarRow;
